822f3c40a9f1e43368fd11514a57b5a4123ada9d
Steven Kwok <derekkwok2020@gmail.com>
2022-06-22 09:22:30 +0100

    Update Dvd.java
    Re-build the Dvd class, also override the toString() function for printing the details

--- dto/Dvd.java
+++ dto/Dvd.java
@@ -1,4 +1,73 @@
 package dto;
 
+import java.util.Date;
+
 public class Dvd {
+    //DVD properties
+    private String title;
+    private Date releaseDate;
+    private String mpaaRating;
+    private String directorName;
+    private String userRating;
+    private String studio;
+
+    // Methods
+
+    //the default toString onyl really serialises the objects class name and hashcode, not useful.
+    //Overriding this method can allow us to print out all of the object's property values instead,
+    //which allows for much faster insight into issues when reading test logs
+    @Override
+    public String toString() {
+        return "Dvd{" + "title=" + title + ", releaseDate=" + releaseDate + ", mpaaRating=" + mpaaRating + ", directorName=" + directorName + ", userRating=" + userRating + ", studio=" + studio + '}';
+    }
+
+    // getters and setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getMpaaRating() {
+        return mpaaRating;
+    }
+
+    public void setMpaaRating(String mpaaRating) {
+        this.mpaaRating = mpaaRating;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public String getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(String userRating) {
+        this.userRating = userRating;
+    }
 }
