52d8108b5af496928d268f7bfa6294943c47c1a3
Steven Kwok <derekkwok2020@gmail.com>
2022-06-22 09:40:54 +0100

    Update DvdLibraryDao.java
    Interface is created

--- dao/DvdLibraryDao.java
+++ dao/DvdLibraryDao.java
@@ -1,4 +1,29 @@
 package dao;
 
+import dto.Dvd;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 public interface DvdLibraryDao {
+
+    Dvd changeReleaseDate(String title, Date releaseDate) throws DvdLibraryDaoException;
+
+    Dvd changeMpaaRating(String title, String mpaaRating) throws DvdLibraryDaoException;
+
+    Dvd changeDirectorName(String title, String directorName) throws DvdLibraryDaoException;
+
+    Dvd changeStudioName(String title, String studioName) throws DvdLibraryDaoException;
+
+    List <Dvd> getAllDvds () throws DvdLibraryDaoException;
+
+    Dvd getDvd(String title) throws DvdLibraryDaoException;
+
+    Map<String, Dvd> getDvdsLastYears(int years) throws DvdLibraryDaoException;
+
+    Map<String, Dvd> getDvdsByMpaaRating(String mpaaRating) throws DvdLibraryDaoException;
+
+    Map<String, Dvd> getDvdsByDirector(String directorName) throws DvdLibraryDaoException;
+
+    Map<String, Dvd> getDvdsByStudio(String studioName) throws DvdLibraryDaoException;
 }
